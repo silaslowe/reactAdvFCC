be2ae81c7412b936bcc84a18e3025cc6551735de
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-10-18 20:08:35 -0500

    finsihed useState at hr 4

---
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+const UseStateBasics = () => {
+  // console.log("hello world")
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler)
+  const [text,setText] = useState('109')
+  const handleClick = () => {
+    if(text === "109"){
+    setText("hello")
+    } else {
+      setText("109")
+    }
+  }
+  return <React.Fragment>
+    <h1>{text}</h1>
+    <button className="btn" onClick={handleClick}>change title</button>
+  </React.Fragment>;
+};
+
+export default UseStateBasics;
