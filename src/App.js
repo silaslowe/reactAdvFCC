0a1b8786c8a367f7106f4502e3d82fb2cd4d0fbe
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-11-10 20:30:06 -0600

    finished with useState section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Setup from './tutorial/1-useState/setup/2-useState-basics'
-// import Final from './tutorial/1-useState/final/2-useState-basics'
+import React from "react"
+// import Setup from "./tutorial/1-useState/setup/4-useState-counter"
+import Final from "./tutorial/1-useState/final/3-useState-array"
 function App() {
   return (
     <div className="container">
-   <Setup/> 
-   {/* <Final/> */}
-   </div>
-  );
+      {/* <Setup /> */}
+      <Final />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random Mesages",
+  })
+  const changeMessage = () => {
+    // Spread operator allows you to target just the property you want to change.
+    setPerson({ ...person, message: "Way Bro", name: "Biff" })
 
-export default UseStateObject;
+    // Each propery of an object can be represented in a seperate variable and the passed using the first param as the value in the html
+    // const [name, setName] = useState("Peter")
+    // const [age, setMessage] = useState(24)
+    // const [message, set] = useState("random message")
+  }
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  )
+}
+
+export default UseStateObject
