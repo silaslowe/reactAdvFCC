62eeaf7ffd8e7cbae3f793d9f7f01f822a5149b2
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-11-11 21:40:36 -0600

    useEffect up thru chapter 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react"
-// import Setup from "./tutorial/1-useState/setup/4-useState-counter"
-import Final from "./tutorial/1-useState/final/3-useState-array"
+import Setup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup"
+// import Final from "./tutorial/1-useState/final/3-useState-array"
 function App() {
   return (
     <div className="container">
-      {/* <Setup /> */}
-      <Final />
+      <Setup />
+      {/* <Final /> */}
     </div>
   )
 }
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react"
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
-};
+  const [size, setSize] = useState(window.innerWidth)
+  useEffect(() => {
+    console.log("useEffect")
+    const checkSize = () => {
+      setSize(window.innerWidth)
+    }
+    window.addEventListener("resize", checkSize)
+    return () => {
+      console.log("cleanup")
+      window.removeEventListener("resize", checkSize)
+    }
+  })
+  console.log("render")
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  )
+}
 
-export default UseEffectCleanup;
+export default UseEffectCleanup
